aee2d44cafee466fb4b075d8f391335874e5c0d4
Duncan <buzzbuzz.macdonald@gmail.com>
2019-01-08 11:40:11 -0800

    camera working

--- DailyFace/ViewController.swift
+++ DailyFace/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var captureSession = AVCaptureSession()
+    var frontCamera: AVCaptureDevice?
+    
+    var photoOutput: AVCapturePhotoOutput?
+    
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
 
     override func viewDidLoad() {
         setupCaptureSession()
@@ -20,23 +27,42 @@ class ViewController: UIViewController {
     }
     
     func setupCaptureSession() {
-        
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
     }
     
     func setupDevice() {
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
         
+        let devices = deviceDiscoverySession.devices
+        
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.front {
+                frontCamera = device
+            }
+        }
     }
     
     func setupInputOutput() {
-        
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: frontCamera!)
+            captureSession.addInput(captureDeviceInput)
+            photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+        } catch {
+            print(error)
+        }
     }
     
     func setupPreviewLayer() {
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraPreviewLayer!.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        cameraPreviewLayer!.frame = self.view.frame
         
+        self.view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
     }
     
     func startRunningCaptureSession() {
-        
+        captureSession.startRunning()
     }
 }
 
